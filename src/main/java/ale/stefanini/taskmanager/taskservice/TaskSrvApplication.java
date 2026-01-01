db2c9930f5e785eecaf2bfbf6b87e3b65c073385
R. Alejandro Fuentes <fuentesra@hotmail.com>
2026-01-01 08:34:07 -0300

    chore: initial project structure and docker infrastructure

---
+++ src/main/java/ale/stefanini/taskmanager/taskservice/TaskSrvApplication.java
@@ -0,0 +1,13 @@
+package ale.stefanini.taskmanager.taskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ale/stefanini/taskmanager/taskservice/TaskSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package ale.stefanini.taskmanager.taskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
